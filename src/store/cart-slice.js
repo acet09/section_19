e505b46928d27426918b6ab85591e8a0c77767ab
acet09 <ysjungman@gmail.com>
2023-05-15 07:18:17 +0900

    264_연습_2

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
@@ -8,21 +8,35 @@ createSlice({
   },
   reducers: {
     addItemToCart(state, action) {
-      const newitem = action.payload;
-      const existingItem = state.items.find(item => item.id === newitem.id);
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newitem.id,
-          price: newitem.price,
+          id: newItem.id,
+          price: newItem.price,
           quantity: 1,
-          totalPrice: newitem.price,
-          name: newitem.title,
+          totalPrice: newItem.price,
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
-        existingItem.totalPrice = existingItem.totalPrice + newitem.price;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() { }
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        // existingItem.tatalPrice = existingItem.totalPrice = existingItem.price;
+      }
+    }
   }
 });
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
