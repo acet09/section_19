337d30c2060fa96bf4b6f5d5e8b30e8e8a5bc1e4
Cactus <ysjungman@gmail.com>
2023-05-16 13:09:14 +0900

    273_데이터_가져오기_2

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -17,7 +17,10 @@ export const fetchCartData = () => {
     };
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData))
+      dispatch(cartActions.replaceCart({
+        items: cartData.items || [],
+        totalQuantity: cartData.totalQuantity
+      }));
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -48,7 +51,10 @@ export const sendCartData = (cart) => {
         'https://react-cover-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
